ca68e235a19029c7b2824030606c934c4e476881
Melissa <m.contrerasr@uniandes.edu.co>
2020-08-22 11:07:19 -0500

    IDENTACION TEST_CSV

--- Test/test_csv.py
+++ Test/test_csv.py
@@ -4,26 +4,26 @@ from DataStructures import listiterator as it
 from DataStructures import liststructure as lt
 
 def test_carga():
-    lista=[]
+    lst=[]
     lst=lt.newList()
 
-    file= "Data/Good reads descarga/books-small.csv"
+    file= "Data/GoodReads/books.csv"
     sep=','
     dialect= csv.excel()
     dialect.delimiter = sep
 
-assert(lt.size(lst)==0),"la lista no empieza en cero"
+    assert(lt.size(lst)==0),"la lista no empieza en cero"
 
-try:
-    with open(file,encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile,dialect=dialect)
+    try:
+        with open(file,encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile,dialect=dialect)
 
-        for row in reader:
-            lista.append(row)
-            lt.addLast(lst,row)
-except:
-    assert False,"Se presento un error al cargar el archivo"
-assert len(lista)== lt.size(lst),"Son diferentes tamaños"
+            for row in reader:
+                lst.append(row)
+                lt.addLast(lst,row)
+    except:
+        assert False,"Se presento un error al cargar el archivo"
+    assert len(lst)== lt.size(lst),"Son diferentes tamaños"
 
-for i in range (len(lista)):
-    assert lt.getElement(lst,i+1)==lista[i],"Las listas no estan en el mismo orden"
+    for i in range (len(lst)):
+        assert lt.getElement(lst,i+1)==lst[i],"Las listas no estan en el mismo orden"
